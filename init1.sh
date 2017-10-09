23c4ae077ef6d3b0b8a4a2d960ab6dd5b7e632c7
nox757 <nox757@gmail.com>
2017-10-09 23:17:26 +0300

    Restruct

--- init1.sh
+++ init1.sh
@@ -1,3 +1,3 @@
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
-sudo /etc/init.d/nginx restart
-sudo /etc/init.d/mysql start
+sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo /etc/init.d/nginx restart 
