b077295954c828779ddfdb9232a5d159356f17a8
nox757 <nox757@gmail.com>
2017-10-10 19:11:17 +0300

    Fixed status in wsgi app

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@
 
 
 def wsgi_application(environ, start_response):
-    status = '200 ОК'
+    #need change ok????????
+    status = '200 OK'
     headers = [ ('Content-Type', 'text/plain') ]
     body = [ bytes(el+'\n', 'ascii') for el in environ['QUERY_STRING'].split('&') ]
     start_response(status, headers)
