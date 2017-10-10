2b62b9acfb0c89d89ca0906a9efe00473415a190
nox757 <nox757@gmail.com>
2017-10-10 14:37:25 +0300

    Repack files to 1.9

--- init.sh
+++ init.sh
@@ -3,6 +3,6 @@ sudo ln -sf /usr/bin/python3 /usr/bin/python
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 cd /home/box/web
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello.py:wsgi_application
