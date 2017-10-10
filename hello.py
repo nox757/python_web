ee4e4f00f6d6d68ba904adde5b063cbf93c2dc71
nox757 <nox757@gmail.com>
2017-10-10 18:43:07 +0300

    Fixed quote

--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@
 def wsgi_application(environ, start_response):
     status = '200 ОК'
     headers = [ ('Content-Type', 'text/plain') ]
-    body = [ bytes(el+'\n', ascii) for el in environ['QUERY_STRING'].split('&') ]
+    body = [ bytes(el+'\n', 'ascii') for el in environ['QUERY_STRING'].split('&') ]
     start_response(status, headers)
 
     return  body
--- init.sh
+++ init.sh
@@ -5,4 +5,4 @@ sudo /etc/init.d/nginx restart
 
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
 #cd /home/box/web
-sudo /home/box/web/gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
