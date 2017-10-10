cd154369943e4205aeed51c0cb3e4edafdda7a3c
nox757 <nox757@gmail.com>
2017-10-10 18:36:14 +0300

    fixed conflict

--- hello.py
+++ hello.py
@@ -2,14 +2,9 @@
 
 
 def wsgi_application(environ, start_response):
-<<<<<<< HEAD
-    status = "200 ОК"
-    headers = [ ("Content-Type", "text/plain") ]
-=======
-    status = bytes("200 ОК", "ascii")
-    headers = [ bytes("Content-Type", "ascii"), bytes("text/plain", "ascii") ]
->>>>>>> origin/master
-    body = [ bytes(el+"\n", ascii) for el in environ["QUERY_STRING"].split("&") ]
+    status = '200 ОК'
+    headers = [ ('Content-Type', 'text/plain') ]
+    body = [ bytes(el+'\n', ascii) for el in environ['QUERY_STRING'].split('&') ]
     start_response(status, headers)
 
     return  body
