7f4f69ecbabd2b5ee4f98391a933b5ae9e8282ef
nox757 <nox757@gmail.com>
2017-10-10 16:59:18 +0300

    Change encode

--- hello.py
+++ hello.py
@@ -2,8 +2,8 @@
 
 
 def wsgi_application(environ, start_response):
-    status = "200 ОК"
-    headers = [ ("Content-Type", "text/plain") ]
+    status = bytes("200 ОК", "ascii")
+    headers = [ bytes("Content-Type", "ascii"), bytes("text/plain", "ascii") ]
     body = [ bytes(el+"\n", ascii) for el in environ["QUERY_STRING"].split("&") ]
     start_response(status, headers)
 
