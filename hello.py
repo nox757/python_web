6eda07b9f5fc488740916cd121ff48ee228907ff
nox757 <nox757@gmail.com>
2017-10-10 14:37:25 +0300

    Repack files to 1.9

--- hello.py
+++ hello.py
@@ -4,8 +4,7 @@
 def wsgi_application(environ, start_response):
     status = "200 ОК"
     headers = [ ("Content-Type", "text/plain") ]
-    QUERY_STRING = environ["QUERY_STRING"].split("&")
-    body = "\n".join(QUERY_STRING)
+    body = [ bytes(el+"\n", ascii) for el in environ["QUERY_STRING"].split("&") ]
     start_response(status, headers)
 
-    return [ body ]
+    return  body
--- init.sh
+++ init.sh
@@ -1,8 +1,8 @@
-sudo ln -sf /usr/bin/python3 /usr/bin/python
+#sudo ln -sf /usr/bin/python3 /usr/bin/python
 
 sudo ln -sf /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
-sudo ln -sf /home/box/etc/hello.py /etc/gunicorn.d/hello.py
-cd /home/box/web
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
+sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+#cd /home/box/web
+sudo /home/box/web/gunicorn -c /etc/gunicorn.d/hello.py hello:wsgi_application
