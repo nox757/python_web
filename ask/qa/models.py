213c765423dd11a5cbf785ec1b7a4267c1b539ef
nox757 <nox757@gmail.com>
2017-12-30 15:43:30 +0300

    Add models step 2.3.*

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    rating = models.IntegerField(default=0)
+    author = models.CharField(max_length=255)
+    likes =models.ManyToManyField(User, related_name='question_like_user')
+    objects = QuestionManager() 
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL )
+    author = models.CharField(max_length=255)
+
+class QuestionManager(models.Manager):
+    def new(self):
+        return self.order_by('-added_at')
+
+    def popular(self):
+        return self.order_by('-rating')
+
